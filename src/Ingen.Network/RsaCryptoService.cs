d01921f975a55eaf733298abae68a928978b9928
ingen084 <ingen188@gmail.com>
2018-09-16 18:59:55 +0900

    RSAも一応実装

---
+++ src/Ingen.Network/RsaCryptoService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Ingen.Network
+{
+	public class RsaCryptoService : ICryptoService, IDisposable
+	{
+		private RSACryptoServiceProvider CryptoServiceProvider { get; }
+		private X509Certificate2 Certificate { get; }
+
+		public RsaCryptoService(string keyFilePath)
+		{
+			Certificate = new X509Certificate2(keyFilePath);
+			CryptoServiceProvider = (RSACryptoServiceProvider)Certificate.PrivateKey;
+		}
+
+		public byte[] Encrypt(byte[] input)
+			=> CryptoServiceProvider.Encrypt(input, true);
+
+		public byte[] Decrypt(byte[] input)
+			=> CryptoServiceProvider.Decrypt(input, true);
+
+		public void Dispose()
+		{
+			CryptoServiceProvider.Dispose();
+			Certificate.Dispose();
+		}
+	}
+}
